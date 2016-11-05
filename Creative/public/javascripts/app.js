78f928c592278df04e5c21d179b65252adbe57ec
CaptBoscho <corbin.byers@gmail.com>
2016-11-05 16:05:38 +0000

    connected

--- Creative/public/javascripts/app.js
+++ Creative/public/javascripts/app.js
@@ -1,8 +1,14 @@
-angular.module('comment', [])
+angular.module('Creative', [])
 .controller('MainCtrl', [
   '$scope',
   function($scope){
     $scope.test = 'Hello world!';
     console.log("hello bro");
+ 
+    $scope.SignUp = function(){
+	var rN = $scope.registerName;
+	console.log(rN);
+    };
   }
+
 ]);
